16b33bcf827b77b57a869b716dff7f69e2f0fd61
Amar <42346300+amarmaulana95@users.noreply.github.com>
2025-08-25 14:47:32 +0700

    Add CI/CD workflow with tests

---
+++ test/users.test.js
@@ -0,0 +1,11 @@
+const request = require('supertest');
+const app = require('../index');
+
+describe('GET /users', () => {
+  it('should return all users', async () => {
+    const res = await request(app).get('/users');
+    expect(res.statusCode).toBe(200);
+    expect(res.body.success).toBe(true);
+    expect(Array.isArray(res.body.users)).toBe(true);
+  });
+});
