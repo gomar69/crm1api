5fa24974dc81513ceed8a1c7b4546ad11a85461a
Amar <42346300+amarmaulana95@users.noreply.github.com>
2025-08-25 14:53:28 +0700

    Add full Node.js + Docker + Jest CI/CD setup

--- index.js
+++ index.js
@@ -1,31 +1,16 @@
 const express = require('express');
+const app = express();
 const pool = require('./db');
 
-const app = express();
-const PORT = 3000;
+app.use(express.json());
 
-// Endpoint test DB
-app.get('/test-db', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT NOW()');
-    res.json({ success: true, db_time: result.rows[0] });
-  } catch (err) {
-    console.error(err);
-    res.json({ success: false, error: err.message });
-  }
-});
-
-// Endpoint untuk ambil semua users
 app.get('/users', async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM users ORDER BY id ASC');
+    const result = await pool.query('SELECT * FROM users');
     res.json({ success: true, users: result.rows });
   } catch (err) {
-    console.error(err);
     res.status(500).json({ success: false, error: err.message });
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
